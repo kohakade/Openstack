7be6ae5a777e670057306b0ab6923e5ce93ecf40
kohakade <abhijeet.kohakade99@gmail.com>
2014-02-17 20:27:02 +0530

    Update file11.sh

--- Swift/file11.sh
+++ Swift/file11.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sudo service swift-account restart
-sudo service swift-object restart
-sudo service swift-container restart
-sudo service swift-proxy restart
+sudo service swift-account stop
+sudo service swift-object stop
+sudo service swift-container stop
+sudo service swift-proxy stop
+
+sudo service swift-account start
+sudo service swift-object start
+sudo service swift-container start
+sudo service swift-proxy start
 
 exit
