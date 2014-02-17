cd69c22c5c869e36759c9c554631b230fb428f21
kohakade <abhijeet.kohakade99@gmail.com>
2014-02-17 14:24:16 +0530

    Create file03.sh

---
+++ Swift/file03.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo fdisk /dev/sdb
+
+sudo partprobe
+
+sudo mkfs.xfs -i size=1024 /dev/sdb1
+
+sudo mkdir /mnt/sdb1
+
+echo "/dev/sdb1 /mnt/sdb1 xfs noatime,nodiratime,nobarrier,logbufs=8 0 0">>/etc/fstab
+
+sudo mount /dev/sdb1
+
+sudo mkdir /mnt/sdb1/{1..4}
+
+sudo chown swift:swift /mnt/sdb1/*
+
+sudo ln -s /mnt/sdb1/{1..4} /srv
+
+sudo mkdir -p /etc/swift/{object-server,container-server,account-server}
+
+for S in {1..4}; do sudo mkdir -p /srv/${S}/node/sdb${S}; done
+
+sudo mkdir -p /var/run/swift
+
+sudo chown -R swift:swift /etc/swift /srv/{1..4}/
+
+sudo sed -i 's/exit 0/mkdir -p /var/run/swift\nchown swift:swift /var/run/swift\nexit 0/g' /etc/rc.local
+
+exit
